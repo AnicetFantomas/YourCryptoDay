606d15cb08efffd99a2999fbb9c1e8928421b6ba
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-05-28 07:52:23 +0200

    Add unit testing

---
+++ src/Test/FetchDetails.test.js
@@ -0,0 +1,10 @@
+import cryptoaReducer, { fetchCryptoDetails } from '../Redux/FetchDtails';
+
+it('fetchCryptoDetails should return a promise', () => {
+    expect(fetchCryptoDetails()).toEqual(expect.any(Function));
+  });
+  
+it('Expect cryptoaReducer to handle getS', () => {
+    expect(cryptoaReducer({}, [fetchCryptoDetails.fulfilled])).not.toBeNull();
+  });
+  
