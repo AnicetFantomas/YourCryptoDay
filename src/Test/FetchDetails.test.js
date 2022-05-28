1538f223e9f0e855932d35689a4239b29820df18
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-05-28 07:57:00 +0200

    Solve linters issues

--- src/Test/FetchDetails.test.js
+++ src/Test/FetchDetails.test.js
@@ -1,10 +1,9 @@
 import cryptoaReducer, { fetchCryptoDetails } from '../Redux/FetchDtails';
 
 it('fetchCryptoDetails should return a promise', () => {
-    expect(fetchCryptoDetails()).toEqual(expect.any(Function));
-  });
-  
+  expect(fetchCryptoDetails()).toEqual(expect.any(Function));
+});
+
 it('Expect cryptoaReducer to handle getS', () => {
-    expect(cryptoaReducer({}, [fetchCryptoDetails.fulfilled])).not.toBeNull();
-  });
-  
+  expect(cryptoaReducer({}, [fetchCryptoDetails.fulfilled])).not.toBeNull();
+});
